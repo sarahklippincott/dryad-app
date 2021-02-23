ec8c9da5615316418b9e74570a736f99d381ade8
Cassiano R. N. dos Santos <caco@posteo.net>
2021-02-23 15:45:34 -0300

    Files previously modified not involved with testing embbeded react

--- testing_prep.sh
+++ testing_prep.sh
@@ -15,7 +15,7 @@ set -e
 
 echo "Initializing database:"
 set -x
-mysql -u root -e 'CREATE DATABASE IF NOT EXISTS dashv2_test'
-mysql -u root -e 'CREATE USER IF NOT EXISTS travis@localhost'
-mysql -u root -e 'GRANT ALL ON dashv2_test.* TO travis@localhost'
+sudo mysql -u root -e 'CREATE DATABASE IF NOT EXISTS dashv2_test'
+sudo mysql -u root -e 'CREATE USER IF NOT EXISTS travis@localhost'
+sudo mysql -u root -e 'GRANT ALL ON dashv2_test.* TO travis@localhost'
 { set +x; } 2>/dev/null
