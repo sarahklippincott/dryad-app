30c59a2c0688320d0cfe72ca8a17b1ede5ef63f8
Ashley Gould <agould@ucop.edu>
2021-10-19 16:46:27 -0700

    lograge: debugging json log generation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # write a second log stream as json
   config.lograge.keep_original_rails_log = true
   config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
   
   Rails.application.default_url_options = { host: 'dryad-dev.cdlib.org' }
 
