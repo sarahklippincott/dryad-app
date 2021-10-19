d3db389020448bd7e6718970ff1ee48f4cb9c99e
Ashley Gould <agould@ucop.edu>
2021-10-19 16:53:36 -0700

    lograge: same

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,9 +58,10 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker   
 
   # write a second log stream as json
-  config.lograge.keep_original_rails_log = true
-  config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
-  config.lograge.formatter = Lograge::Formatters::Logstash.new
+  config.lograge.enabled = true
+  #config.lograge.keep_original_rails_log = true
+  #config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+  #config.lograge.formatter = Lograge::Formatters::Logstash.new
   
   Rails.application.default_url_options = { host: 'dryad-dev.cdlib.org' }
 
